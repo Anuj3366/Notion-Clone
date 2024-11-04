799c89738172510b9e9e64ea16a8d32242c717ab
Anuj3366 <anujgarg3366@gmail.com>
2024-11-04 22:27:43 +0530

    add Tailwind CSS configuration and update layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
         {children}
       </body>
     </html>
